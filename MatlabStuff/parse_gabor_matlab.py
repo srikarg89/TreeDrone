7de7eb605c63a42282a255c2a30df2280aa24e23
Srikar Gouru <srikarg89@gmail.com>
2020-01-18 11:23:29 -0500

    Parse gabor matlab output

---
+++ MatlabStuff/parse_gabor_matlab.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+
+shape = (288,512,26)
+
+npfile_dir = 'Numpy_files/'
+gabor_output = np.load(npfile_dir + 'SafewayGabor1.npy')
+print(gabor_output.shape)
+gabor_output = gabor_output.reshape(shape)
+np.savetxt('CSV_files/SafewayGabor1.csv', gabor_output, delimiter=',')
+#cv2.imwrite("Images/Gaborfilter.png", gabor_output)
--- MatlabStuff/pca.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import cv2
-
-npfile_dir = 'Numpy_files/'
-gabor_output = np.load(npfile_dir + 'SafewayGabor1.npy')
-print(gabor_output.shape)
-
