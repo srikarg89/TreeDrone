5b71e5dab92fc7dcd8a49b26bf173ca48a9816e9
Srikar Gouru <srikarg89@gmail.com>
2020-01-22 18:47:54 -0500

    Added files to RealCVImages

--- split_video.py
+++ split_video.py
@@ -1,18 +1,18 @@
 import cv2
 import os
 
-#filename = "WindsorNationalPark"
 #filename = "Nashville"
-filename = 'Bush/vid3'
-ending = ".avi"
-#ending = ".webm"
+#filename = 'Bush/vid3'
+#filename = "Westgate1"
+filename = "Westgate2"
+ending = ".mp4"
 savename = "Frame"
 os.mkdir("TestImages/" + filename)
 cap = cv2.VideoCapture("Videos/" + filename + ending)
 
 fails = 0
 framenum = 0
-saverate = 30
+saverate = 5
 while cap.isOpened():
     if fails == 100:
         break
@@ -21,7 +21,8 @@ while cap.isOpened():
         if framenum % saverate == 0:
             print(framenum)
 #            cv2.imwrite("TestImages/" + filename + "/" + savename + str(framenum // saverate) + ".png", frame)
-            cv2.imwrite("TestImages/Bush/" + savename + str(framenum // saverate) + ".png", frame)
+#            cv2.imwrite("TestImages/Bush/" + savename + str(framenum // saverate) + ".png", frame)
+            cv2.imwrite("TestImages/Westgate2/" + savename + str(framenum // saverate) + ".png", frame)
         fails = 0
         framenum += 1
     else:
--- test.py
+++ test.py
@@ -1,19 +1,4 @@
 import cv2
 
-vid = cv2.VideoCapture('D:/video/20200112-160256-704278-0.avi')
-framecount = 0
-while vid.isOpened():
-    ret, frame = vid.read()
-    if ret == False:
-        continue
-    framecount += 1
-    if framecount % 20 == 0:
-        print(framecount)
-    if framecount == 200:
-        cv2.imshow("Image", cv2.resize(frame, (3840 // 6, 2160 // 6)))
-        cv2.imwrite("TestFrame.jpg", frame)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        break
-
-vid.close()
+regular = cv2.imread("C:/Users/Srikar/Downloads/regular.jpg")
+regular = cv2.imread("C:/Users/Srikar/Downloads/regular.jpg")
