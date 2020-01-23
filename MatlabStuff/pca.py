cb2a99f8194933dddc7485b805f331dc3798bc5e
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2020-01-22 22:59:27 -0500

    Reorganization of repo

--- MatlabStuff/pca.py
+++ MatlabStuff/pca.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-#%config InlineBackend.figure_format='retina'
+%config InlineBackend.figure_format='retina'
+
 
 # Load in the data
-df = pd.read_csv('CSV_files/SafewayGabor1.csv')
+df = pd.read_csv('2013_2014_cleaned.csv')
 
 # Standardize the data to have a mean of ~0 and a variance of 1
 X_std = StandardScaler().fit_transform(df)
@@ -23,10 +24,5 @@ plt.xlabel('PCA features')
 plt.ylabel('variance %')
 plt.xticks(features)
 
-plt.show()
 # Save components to a DataFrame
 PCA_components = pd.DataFrame(principalComponents)
-
-#plt.scatter(PCA_components[0], PCA_components[1], alpha=.1, color='black')
-#plt.xlabel('PCA 1')
-#plt.ylabel('PCA 2')
--- test.py
+++
@@ -1,4 +0,0 @@
-import cv2
-
-regular = cv2.imread("C:/Users/Srikar/Downloads/regular.jpg")
-regular = cv2.imread("C:/Users/Srikar/Downloads/regular.jpg")
